91c628076b563edcd6b2fc3d69931d917775dd3f
CurtisGrayeBabin <curtsocal@gmail.com>
2021-07-16 22:59:38 -0700

    added footer section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,11 @@ export class AppComponent {
   title: string = 'my-store';
   storeName: string = "Curtis's Store: The Pinnacle of E-Commerce™";
 
+  linkedinImg: string = "https://image.flaticon.com/icons/png/512/174/174857.png";
+  githubImg: string = "https://image.flaticon.com/icons/png/512/25/25231.png";
+  
+  linkedinUrl: string = "https://www.linkedin.com/in/curtisbabin/";
+  repoUrl: string = "https://github.com/CurtisGrayeBabin/my-store";
+
   constructor(){}
  }
